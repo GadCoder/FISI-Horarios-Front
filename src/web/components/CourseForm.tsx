945c14085e951a7f1a9099532338b72cb509d637
Jasmin Amparo Huerta Villalta <jasmin.huerta@culqi.com>
2024-06-27 15:07:47 -0500

    fix: change name api

--- src/web/components/CourseForm.tsx
+++ src/web/components/CourseForm.tsx
@@ -8,7 +8,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
 
-import { getCoursesFromSemester, getSectionsFromCourse, getSchedulesFromSection } from "../../../app/api"
+import { getCoursesFromSemester, getSectionsFromCourse, getSchedulesFromSection } from "../../adapters/controllers/apiController"
 
 type ScheduleData = {
   dia: string,
